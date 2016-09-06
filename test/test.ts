dfceda3ffd297c043fc334fa26d32d2da592edf4
sfali23 <f.syed.ali@gmail.com>
2016-09-05 23:15:27 -0400

    working test

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,20 @@
 /// <reference path="../typings/index.d.ts" />
 /// <reference path="./bundle.d.ts" />
 
-import {WebDriver, Browser, By} from 'selenium-webdriver';
+import {WebDriver, Browser} from 'selenium-webdriver';
 import webdriver = require('selenium-webdriver');
 
-let driver: WebDriver = new webdriver.Builder().forBrowser(Browser.CHROME).build();
+let by = webdriver.By,
+    until = webdriver.until;
 
-let until = webdriver.until;
+let driver: WebDriver = new webdriver.Builder().forBrowser(Browser.CHROME).usingServer('http://localhost:4444/wd/hub').build();
 
 driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.name('btnG')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+driver.findElement(by.name('q')).sendKeys('webdriver');
+driver.findElement(by.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 100000).then(function (result: boolean) {
+    setTimeout(function () {
+        console.log("Quiting");
+        driver.quit();
+    }, 10000);
+});
